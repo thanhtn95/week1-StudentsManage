07d2046b3a06ee27b116689551f43c6d9d20199a
zonesama <trannhatthanh31@gmail.com>
2019-10-30 09:46:25 +0700

    First Commit.

--- src/StudentsManager/Main.java
+++ src/StudentsManager/Main.java
@@ -6,10 +6,11 @@ public class Main {
         studentsList[0] = new Student(1,"PEPEG","OMEGALUL","0846546556465",5);
         Manager admin = new Manager();
         admin.add(new Student(2,"KEKW","OMEGALUL","09845664",10));
-        System.out.println(studentsList.length);
+        admin.add(new Student(3,"KEKW","OMEGALUL","09845664",2));
+        System.out.println("Number of Student: "+studentsList.length);
         admin.deleteStudent(1);
-
-        System.out.println(admin.getAverageGradeOfStudents());
+        System.out.println("Number of Student: "+studentsList.length);
+        System.out.println("Average Score: "+admin.getAverageGradeOfStudents());
     }
 
 }
